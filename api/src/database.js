21692b8822f3d7bc6217e4f3b7993dcc0d79fe20
tspdevops <tspdevops@github.com>
2021-08-10 14:02:59 -0300

    Lo esencial

--- api/src/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://mongo/landingPageDB')
-  .then(db=> console.log('Db is connected to ', db.connection.host))
-  .catch(err => console.error(err))
